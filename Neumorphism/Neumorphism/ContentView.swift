8ce01042d2c135f795747c74cbad78552a0cc2dd
touyou <fujiyou1121@gmail.com>
2020-03-04 13:10:14 +0900

    till first of inner shadow (neumorphism)

---
+++ Neumorphism/Neumorphism/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by 藤井陽介 on 2020/03/04.
+//  Copyright © 2020 touyou. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+}
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    }
+            })
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.offWhite
+            VStack(spacing: 50) {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 300)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                Button(action: {
+                    print("Button tapped")
+                }) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                }
+                .buttonStyle(SimpleButtonStyle())
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
