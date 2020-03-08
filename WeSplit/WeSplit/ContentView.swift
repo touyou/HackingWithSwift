d2b9c286f735b4dea4bc65baca5d9207e7c41756
touyou <fujiyou1121@gmail.com>
2020-03-08 18:04:15 +0900

    [100Days] WIP Creating a form

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 藤井陽介 on 2020/03/08.
+//  Copyright © 2020 touyou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
